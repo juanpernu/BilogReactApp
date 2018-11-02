647d26cfe02aad5b2b3d7a869181c8b2e8381b62
juan pernumian <juan.pernumian@mercadolibre.com>
2018-11-02 18:00:45 -0300

    create api folder

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const qs = require('qs');
+const axios = require('axios');
+import Config from "./configs";
+
+export function obtenerPermisos(bodyRequest) {
+
+  axios.post(`${Config.baseBilogURL}/ObtenerPermisos`, qs.stringify(bodyRequest), {headers: { 'Content-Type': 'application/x-www-form-urlencoded' }})
+  .then((res) => {
+    // Use Fast-xml-parser here
+    let data = res.data.split('>');
+    data = data[2].replace(/([</]+)\w+/, '');
+    data = JSON.parse(data)
+    console.log(data.response);
+  })
+  .catch((err) => {
+    // Do somthing
+  })
+
+}
